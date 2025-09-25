7f4c8daffb6c6095e17acfb5bde40ffcc5c13844
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-09-25 10:55:52 +0100

    fixed in world strainer recipe being effected by alltheore mass hammer recipe removals

--- kubejs/server_scripts/mods/AllTheOres.js
+++ kubejs/server_scripts/mods/AllTheOres.js
@@ -3,6 +3,10 @@
 ServerEvents.recipes(event => {
 
     //Remove
-    event.remove({type: "shaped", input: '#alltheores:ore_hammers' })
+    event.remove({id: /alltheores:crafting\/hammer/})
+    event.remove({id: /alltheores:crafting\/.*\/plate/})
+    event.remove({id: /alltheores:crafting\/.*\/rod/})
+    event.remove({id: 'alltheores:crafting/enderium/alloy_blending_from_dust'})
+    event.remove({id: 'alltheores:crafting/steel/alloy_blending_from_dust'})
 
 })
